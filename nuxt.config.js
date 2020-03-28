6e914aaf0753064bc94ae43f482578d8d6f84d57
Nicklas Kevin Frank <nicklas@monori.dk>
2020-03-28 22:45:31 +0100

    Updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,13 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Laravel Konsulent',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Få adgang til samme rådgivning og kompetencer som danmarks største brands | Laravel Konsulent' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: []
   },
   /*
   ** Customize the progress-bar color
