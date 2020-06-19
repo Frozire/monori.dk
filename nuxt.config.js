02766ac2a1d899c53922871ae1e87a3567dd1efc
Nicklas Kevin Frank <nicklas@monori.dk>
2020-06-19 11:36:45 +0200

    Ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,100 +24,96 @@ export default {
     ],
     script: [
       {
-        json: `
-        {
+        json: {
           '@context': 'https://schema.org/',
           '@type': 'WebSite',
           name: 'Laravel Konsulent ved Nicklas K. Frank',
           url: 'https://laravelkonsulent.dk'
-        }
-        `,
-        type: 'application/ld+json'
-      },
-      {
-        json: `{
-        '@context': 'https://schema.org/',
-        '@type': 'Product',
-        name: 'Laravel rådgivning og udvikling',
-        image: '',
-        description: 'Erfaren og pålidelig rådgivning - Sikker og professionel udvikling - Løsninger der passer til dine behov',
-        brand: 'Nicklas K. Frank',
-        offers: {
-          '@type': 'Offer',
-          url: 'https://laravelkonsulent.dk/#priser',
-          priceCurrency: 'DKK',
-          price: '899',
-          availability: 'https://schema.org/InStock',
-          itemCondition: 'https://schema.org/NewCondition'
-        }
-        `,
-        type: 'application/ld+json'
-      },
-      {
-        json: `
-        {
-        '@context': 'https://schema.org/',
-        '@type': 'Person',
-        name: 'Nicklas K. Frank',
-        url: 'https://laravelkonsulent.dk/#ommig',
-        image: 'https://laravelkonsulent.dk/nicklasfrank.jpg',
-        sameAs: [
-          'https://linkedin.com/in/nkfrank',
-          'https://github.com/Frozire'
-        ],
-        jobTitle: 'Senior Web Developer',
-        worksFor: {
-          '@type': 'Organization',
-          name: 'Aller Media A/S'
-        }
-        }
-        `,
-        type: 'application/ld+json'
-      },
-      {
-        json: `
-        {
-        '@context': 'https://schema.org',
-        '@type': 'Corporation',
-        name: 'Laravel Konsulent ved Nicklas K. Frank',
-        alternateName: 'Monori',
-        url: 'https://laravelkonsulent.dk',
-        logo: '',
-        sameAs: 'https://www.facebook.com/monori.dk'
-        }`,
-        type: 'application/ld+json'
-      },
-      {
-        json: `{
-        '@context': 'https://schema.org',
-        '@type': 'ProfessionalService',
-        name: 'Laravel Konsulent ved Nicklas K. Frank',
-        image: 'https://laravelkonsulent.dk/nicklasfrank.jpg',
-        url: 'https://laravelkonsulent.dk',
-        telephone: '(+45) 53 57 16 13',
-        priceRange: '899',
-        address: {
-          '@type': 'PostalAddress',
-          addressLocality: 'Copenhagen',
-          addressCountry: 'DK'
         },
-        openingHoursSpecification: {
-          '@type': 'OpeningHoursSpecification',
-          dayOfWeek: [
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday'
+        type: 'application/ld+json'
+      },
+      {
+        json: {
+          '@context': 'https://schema.org/',
+          '@type': 'Product',
+          name: 'Laravel rådgivning og udvikling',
+          image: '',
+          description: 'Erfaren og pålidelig rådgivning - Sikker og professionel udvikling - Løsninger der passer til dine behov',
+          brand: 'Nicklas K. Frank',
+          offers: {
+            '@type': 'Offer',
+            url: 'https://laravelkonsulent.dk/#priser',
+            priceCurrency: 'DKK',
+            price: '899',
+            availability: 'https://schema.org/InStock',
+            itemCondition: 'https://schema.org/NewCondition'
+          }
+        },
+        type: 'application/ld+json'
+      },
+      {
+        json:
+        {
+          '@context': 'https://schema.org/',
+          '@type': 'Person',
+          name: 'Nicklas K. Frank',
+          url: 'https://laravelkonsulent.dk/#ommig',
+          image: 'https://laravelkonsulent.dk/nicklasfrank.jpg',
+          sameAs: [
+            'https://linkedin.com/in/nkfrank',
+            'https://github.com/Frozire'
           ],
-          opens: '00:00',
-          closes: '23:59'
+          jobTitle: 'Senior Web Developer',
+          worksFor: {
+            '@type': 'Organization',
+            name: 'Aller Media A/S'
+          }
         },
-        sameAs: 'https://www.facebook.com/monori.dk'
-        }
-        `,
+        type: 'application/ld+json'
+      },
+      {
+        json:
+        {
+          '@context': 'https://schema.org',
+          '@type': 'Corporation',
+          name: 'Laravel Konsulent ved Nicklas K. Frank',
+          alternateName: 'Monori',
+          url: 'https://laravelkonsulent.dk',
+          logo: '',
+          sameAs: 'https://www.facebook.com/monori.dk'
+        },
+        type: 'application/ld+json'
+      },
+      {
+        json: {
+          '@context': 'https://schema.org',
+          '@type': 'ProfessionalService',
+          name: 'Laravel Konsulent ved Nicklas K. Frank',
+          image: 'https://laravelkonsulent.dk/nicklasfrank.jpg',
+          url: 'https://laravelkonsulent.dk',
+          telephone: '(+45) 53 57 16 13',
+          priceRange: '899',
+          address: {
+            '@type': 'PostalAddress',
+            addressLocality: 'Copenhagen',
+            addressCountry: 'DK'
+          },
+          openingHoursSpecification: {
+            '@type': 'OpeningHoursSpecification',
+            dayOfWeek: [
+              'Monday',
+              'Tuesday',
+              'Wednesday',
+              'Thursday',
+              'Friday',
+              'Saturday',
+              'Sunday'
+            ],
+            opens: '00:00',
+            closes: '23:59'
+          },
+          sameAs: 'https://www.facebook.com/monori.dk'
+        },
         type: 'application/ld+json'
       }
     ],
