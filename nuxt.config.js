5eb960ef10eb3b29e6d170d6d1f9249f8362562e
Nicklas Kevin Frank <nicklas@monori.dk>
2020-10-05 20:34:35 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -128,6 +128,15 @@ export default {
     '@nuxt/content',
   ],
 
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
+
   // Content module configuration (https://go.nuxtjs.dev/content-config)
   content: {},
 
