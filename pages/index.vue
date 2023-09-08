f1e5424ae0c703eb84739d65a725348cc28013fc
Nicklas Kevin Frank <Frozire@users.noreply.github.com>
2023-09-08 19:26:11 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
       <div class="max-w-screen-xl mx-auto">
         <div class="relative flex z-20 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <div
-            class="mt-10 justify-between mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+            class="mt-10 justify-between mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-12 xl:mt-28"
           >
             <div class="sm:text-center lg:text-left">
               <div class="mt-12 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-7 lg:text-left">
